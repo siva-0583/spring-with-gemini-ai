395c80e771feaf2cec5ed4cb39e0690615917653
siva <2000031587@kluniversity.in>
2025-09-14 00:50:27 +0530

    Description : Updated EmailGeneratorController Author : Siva

--- src/main/java/com/email/writer/ai/controller/EmailGeneratorController.java
+++ src/main/java/com/email/writer/ai/controller/EmailGeneratorController.java
@@ -1,6 +1,7 @@
 package com.email.writer.ai.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmailGeneratorController {
 
 	private final EmailGeneratorService emailGeneratorService;
